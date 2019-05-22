5de453ab2c5ff0cc68046bd11be59baab9798298
Marto Nieto Guerrero <marto.nieto.g16@gmail.com>
2019-05-21 21:43:36 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+### Programa Simple Desarrollado Con Python, Flask y MongoDB
+
+##### Instrucciones 
+
+Descargar python desde https://www.python.org/downloads/ 
+Descargar MongoDB desde https://www.mongodb.com/download-center#community 
+
+##### Instalar Flask, bson & pymongo 
+
+pip install Flask  
+pip install bson 
+pip install pymongo 
+
+##### Descargar / clonar el código fuente de Github
+
+https://github.com/marto-nieto-g16/Activity-Reminder.git
+
+##### Ejecutar
+
+Run app.py  
+
+y/o
+
+python app.py
+
+Nuestro servidor web local se ejecuta en el puerto 5000 por defecto. 
+
+##### Activity Reminder !!!
---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template,request,redirect,url_for  
+from bson import ObjectId  
+from pymongo import MongoClient
+from datetime import datetime, date, timedelta
+import os
+
+
+app = Flask(__name__)
+title = "Activity Reminder"
+heading = "Recordatorio De Actividades"
+
+client = MongoClient("mongodb://127.0.0.1:27017") #host uri
+db = client.mymongodb    #Select the database
+todos = db.todo #Select the collection name
+
+
+def Date(x):
+	fecha = datetime.now().date()
+	fecha_final = datetime.strptime(x, '%Y-%m-%d') 
+	result = "" 
+	if str(fecha_final) <  str(fecha)+" 00:00:00": 
+		result = "Es menor"
+	elif str(fecha_final) == str(fecha)+" 00:00:00" : 
+		result = "fecha actual"
+	else:
+		result = "Es mayor"
+	return result		
+
+def redirect_url():
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for('index')
+
+@app.route("/list")
+def lists ():
+	#Display the all Tasks
+	todos_l = todos.find()
+	a1="active"
+	return render_template('index.html',a1=a1,todos=todos_l,t=title,h=heading)
+
+@app.route("/")
+@app.route("/uncompleted")
+def tasks ():
+	#Display the Uncompleted Tasks
+	todos_l = todos.find({"done":"no"})
+	a2="active"
+	return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading)
+
+
+@app.route("/completed")
+def completed ():
+	#Display the Completed Tasks
+	todos_l = todos.find({"done":"yes"})
+	a3="active"
+	return render_template('index.html',a3=a3,todos=todos_l,t=title,h=heading)
+
+@app.route("/done")
+def done ():
+	#Done-or-not ICON
+	id=request.values.get("_id")
+	task=todos.find({"_id":ObjectId(id)})
+	if(task[0]["done"]=="yes"):
+		todos.update({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
+	else:
+		todos.update({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
+	redir=redirect_url()	
+
+	return redirect(redir)
+
+@app.route("/action", methods=['POST'])
+def action ():
+	#Adding a Task
+	name=request.values.get("name")
+	desc=request.values.get("desc")
+	date=request.values.get("date")
+	pr=request.values.get("pr")
+	todos.insert({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"yes"})	
+	return redirect("/list")
+
+@app.route("/remove")
+def remove ():
+	#Deleting a Task with various references
+	key=request.values.get("_id")
+	todos.remove({"_id":ObjectId(key)})
+	return redirect("/list")
+
+@app.route("/update")
+def update ():
+	id=request.values.get("_id")
+	task=todos.find({"_id":ObjectId(id)})
+	return render_template('update.html',tasks=task,h=heading,t=title)
+
+@app.route("/action3", methods=['POST'])
+def action3 ():
+	#Updating a Task with various references
+	name=request.values.get("name")
+	desc=request.values.get("desc")
+	date=request.values.get("date")
+	pr=request.values.get("pr")
+	id=request.values.get("_id")
+	todos.update({"_id":ObjectId(id)}, {'$set':{ "name":name, "desc":desc, "date":date, "pr":pr }})
+	return redirect("/update?_id="+id)
+
+@app.route("/search", methods=['GET'])
+def search():
+	#Searching a Task with various references
+
+	key=request.values.get("key")
+	refer=request.values.get("refer")
+	if(key=="_id"):
+		todos_l = todos.find({refer:ObjectId(key)})
+	else:
+		todos_l = todos.find({refer:key})
+	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
+
+if __name__ == "__main__":
+
+    app.run(debug=True, host="192.168.1.67", port=5000)
